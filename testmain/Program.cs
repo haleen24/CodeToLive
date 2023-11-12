3f227a4eadfa6b69a38787577062912caf4a6473
stepanov <rastepanov@edu.hse.ru>
2023-11-12 15:59:52 +0300

    один из тестовых файлов полетел. Исправил

--- testmain/Program.cs
+++ testmain/Program.cs
@@ -1,4 +1,4 @@
 using LexerSpace;
 
-Lexer lex = new Lexer("../../../../LexerTest/TestFiles/test_4.txt");
+Lexer lex = new Lexer("../../../../LexerTest/TestFiles/KeywordsTest.txt");
 Console.WriteLine(String.Join('\n',lex.Lex()));
