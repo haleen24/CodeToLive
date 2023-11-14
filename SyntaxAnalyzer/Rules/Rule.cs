ad9108db534d5f9015c048aa283c8d7e2b4afbc0
TimurTimergalin <timur20041229@gmail.com>
2023-11-14 14:42:12 +0300

    All grammar unit types listed

---
+++ SyntaxAnalyzer/Rules/Rule.cs
@@ -0,0 +1,21 @@
+﻿using LexerSpace;
+using SyntaxAnalyzer.Nodes;
+using SyntaxAnalyzer.Parsers;
+
+namespace SyntaxAnalyzer.Rules;
+
+public class Rule
+{
+    public ParserFactory Factory { get; }
+    public NodeConstructor Constructor { get; }
+
+    public Rule(ParserFactory factory, NodeConstructor constructor)
+    {
+        Factory = factory;
+        Constructor = constructor;
+    }
+    
+    private static INode Identical(IParser parser) => parser[0]; // Самый простой NodeConstructor
+
+    public static Rule Alternative(params GrammarUnit[] args) => new Rule(() => new Alternative(args), Identical);
+}
