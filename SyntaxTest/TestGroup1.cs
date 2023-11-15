326f970d577f649ee89871509d507bf5fa5f92f6
TimurTimergalin <timur20041229@gmail.com>
2023-11-15 22:31:11 +0300

    Source files for testing added TreeTester class added

---
+++ SyntaxTest/TestGroup1.cs
@@ -0,0 +1,15 @@
+namespace SyntaxTest;
+
+public class TestGroup1
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
---
+++ SyntaxTest/TreeTester.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using SyntaxAnalyzer.Nodes;
+
+namespace SyntaxTest;
+
+public interface ITreeTester
+{
+    public bool Test(INode? node);
+}
+
+public delegate bool FieldsTester(dynamic x);
+
+public class TreeTester<T> : ITreeTester
+    where T : INode
+{
+    private FieldsTester? Tester { get; }
+    private ITreeTester[] ChildrenTesters { get; }
+
+    public TreeTester(FieldsTester? fieldsTester = null, params ITreeTester[] testers)
+    {
+        Tester = fieldsTester;
+        ChildrenTesters = testers;
+    }
+
+    public bool Test(INode? node)
+    {
+        if (node is not T)
+        {
+            return false;
+        }
+
+        if (Tester != null && !Tester!.Invoke(node))
+        {
+            return false;
+        }
+
+        int n = ChildrenTesters.Length;
+        int i = 0;
+
+        foreach (INode? node1 in node.Walk())
+        {
+            if (i >= n)
+            {
+                return false;
+            }
+
+            if (!ChildrenTesters[i].Test(node1))
+            {
+                return false;
+            }
+        }
+
+        return i == n;
+    }
+}
+
+public class NullTester : ITreeTester
+{
+    public bool Test(INode? node) => node == null;
+}
